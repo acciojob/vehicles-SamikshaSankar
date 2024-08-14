0499989c4410018ec79a8cdb5475584fc4865d50
Samiksha sandip sankar <samikshasankar2000@gmail.com>
2024-08-15 00:27:02 +0530

    Adding Boat.java

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,16 @@
+public class Boat implements WaterVehicle {
+    private String name;
+    private int capacity;
+    
+    //constructor
+    public Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
+    public String getVehicleName(){
+        return name;
+    }
+    public int getVehicleCapacity(){
+        return capacity;
+    }
+}
